9da08030dce35310f7b66c986948c6d7a25733cb
FlyinPandaa <mikefang456@gmail.com>
2024-03-09 20:51:45 -0800

    Landing Page done

--- src/components/landing.js
+++ src/components/landing.js
@@ -3,14 +3,17 @@ import './landing.css'
 
 function Landing(){
     return(
-    <body>        
+    <body>
+        
         <section class="hero">
             <div class="hero-content">
+                <img src="https://cdn.discordapp.com/attachments/1215849410555150447/1216234168078569572/output-onlinepngtools.png?ex=65ffa556&is=65ed3056&hm=34c5142e16694530b619f8689f8fc7de54be39abe435180187e4609fb2ca740c&" alt="Your Logo" class="logo"></img>
                 <h1>Welcome to SoilMates</h1>
                 <p>The best AI-powered gardening buddy to have.</p>
                 <a href="#" class="hero-cta">Get Started</a>
             </div>
         </section>
+
         <section class="features">
             <h2>Features</h2>
             <div class="feature-list">
@@ -31,6 +34,7 @@ function Landing(){
                 </div>
             </div>
         </section>
+
     </body>
     )
 }
